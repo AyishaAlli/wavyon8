24d557e4f9fe9b21a1a90813e0ecf918e7a1caec
Ayisha Alli <alli.ayisha1@gmail.com>
2023-04-12 18:41:26 +0100

    added about us title

--- wavyon8-app/src/pages/AboutUsPage.jsx
+++ wavyon8-app/src/pages/AboutUsPage.jsx
@@ -4,16 +4,16 @@ import AboutUs from "../components/About/AboutUs";
 
 function AboutUsPage() {
   return (
-    <div>
-      <div>
+    <>
+    <h1 style={{paddingTop:'200px', textAlign:'center'}}>ABOUT US</h1>
         <Flush />
         <AboutUs/>
-      </div>
+
       {/* <span>
         Would you like to Donate?
         <a href="https://www.paypal.com/paypalme/wavyon8">Click Here!</a>
       </span> */}
-    </div>
+    </>
   );
 }
 
